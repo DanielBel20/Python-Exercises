2ba7c26e2e84de4a6d2b48a82c6ef6927bc5b82d
DanielBelgun <dan_1st@yahoo.com>
2022-04-17 21:40:51 +0300

    updates

--- tema5_12_append_2_string_values_from_keys_together.py
+++ tema5_12_append_2_string_values_from_keys_together.py
@@ -1,10 +1,11 @@
-"""Modify and return the given map as follows: for this problem
+""" Modify and return the given map as follows: for this problem
 the map may or may not contain the "a" and "b" keys.
 If both keys are present, append their 2 string values together
 and store the result under the key "ab".
 {"b": "There", "a": "Hi"} → {"b": "There", "a": "Hi", "ab": "HiThere"}
 {"a": "Hi"} → {"a": "Hi"}
-{"b": "There"} → {"b": "There"}"""
+{"b": "There"} → {"b": "There"} """
+
 dictionary = {"b": "There", "a": "Hi"}
 if "a" in dictionary.keys() and "b" in dictionary.keys():
     dictionary["ab"] = dictionary.get("a") + dictionary.get("b")
